d5661ad142828484cfeab2e65a516cd556726ce4
rongjieaaa <rongjieaaa@gmail.com>
2020-09-07 12:29:58 +0800

    ToDoList

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+      <div class="todo-head">
+        {{title}}
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      list: "",
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
