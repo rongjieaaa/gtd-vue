b2fd3ac412e4c7fdd40ae29d8638e3893c9320b8
rongjieaaa <rongjieaaa@gmail.com>
2020-09-07 13:34:04 +0800

    实现简单的子项增加、编辑、移除功能。

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,20 +1,77 @@
 <template>
-  <div class="hello">
-      <div class="todo-head">
-        {{title}}
+
+  <div class="todo-content">
+
+    <!--头部-->
+    <div class="todo-head">
+      {{todo.title}}
+    </div>
+
+    <!--增加子项-->
+    <div class="todo-addItem">
+      <button @click="addItem">增加子项</button>
+    </div>
+
+    <!--未完成的-->
+    <div class="todo-uncompleted">
+      <div class="todo-uncompleted-title">未完成的：</div>
+      <div class="todo-uncompleted-item" v-for="item in todo.items" :key="item.uuid">
+        <template v-if="!item.deleted && !item.completed">
+          <input type="checkbox" v-model="item.completed" />
+          <input v-model="item.title" />
+          <button @click="removeItem(item)" >移除</button>
+        </template>
       </div>
+    </div>
+
+    <!--已完成的-->
+    <div class="todo-completed">
+      <div class="todo-completed-title">已完成的：</div>
+      <div class="todo-completed" v-for="item in todo.items" :key="item.uuid">
+        <template v-if="!item.deleted && item.completed">
+          <input type="checkbox" v-model="item.completed" />
+          <input v-model="item.title" />
+          <button @click="removeItem(item)" >移除</button>
+        </template>
+      </div>
+    </div>
 
   </div>
+
 </template>
 
 <script>
+import UUID from "uuid";
 export default {
   data() {
     return {
-      title: "",
-      list: "",
+      todoList: [
+        {
+          title: "todoList",
+          items: [],
+          completed: [],
+        }
+      ],
+      todo: undefined,
     }
   },
+  created() {
+    if(this.todoList && this.todoList.length && !this.todo) {
+      this.todo = this.todoList[0];
+    }
+  },
+  methods: {
+    addItem() {
+      this.todo.items.push({
+        uuid: UUID.v4().split("-").join(""),
+        completed: false,
+        deleted: false,
+      })
+    },
+    removeItem(item) {
+      item.deleted = true;
+    },
+  }
 }
 </script>
 
