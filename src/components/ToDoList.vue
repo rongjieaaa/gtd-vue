506cc04bf294e889a562d0f6feea893fa94e6a5a
rongjieaaa <rongjieaaa@gmail.com>
2020-09-10 13:32:55 +0800

    完善样式、尝试使用element

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,37 +1,33 @@
 <template>
 
-  <div class="todo-content">
+  <div>
 
     <!--头部-->
     <div class="todo-head">
       {{todo.title}}
-    </div>
-
-    <!--增加子项-->
-    <div class="todo-addItem">
-      <button @click="addItem">增加子项</button>
+      <el-button class="todo-button" size="mini" icon="el-icon-plus" circle @click="addItem"></el-button>
     </div>
 
     <!--未完成的-->
-    <div class="todo-uncompleted">
-      <div class="todo-uncompleted-title">未完成的：</div>
-      <div class="todo-uncompleted-item" v-for="item in todo.items" :key="item.uuid">
+    <div>
+      <div>未完成的：</div>
+      <div v-for="item in todo.items" :key="item.uuid">
         <template v-if="!item.deleted && !item.completed">
-          <input type="checkbox" v-model="item.completed" />
-          <input v-model="item.title" />
-          <button @click="removeItem(item)" >移除</button>
+          <el-button class="todo-button" size="mini" icon="el-icon-check" circle @click="item.completed = true;"></el-button>
+          <el-input class="todo-title" size="small" v-model="item.title"></el-input>
+          <el-button class="todo-button" size="mini" icon="el-icon-delete" circle @click="removeItem(item)"></el-button>
         </template>
       </div>
     </div>
 
     <!--已完成的-->
-    <div class="todo-completed">
-      <div class="todo-completed-title">已完成的：</div>
-      <div class="todo-completed" v-for="item in todo.items" :key="item.uuid">
+    <div>
+      <div>已完成的：</div>
+      <div v-for="item in todo.items" :key="item.uuid">
         <template v-if="!item.deleted && item.completed">
-          <input type="checkbox" v-model="item.completed" />
-          <input v-model="item.title" />
-          <button @click="removeItem(item)" >移除</button>
+          <el-button class="todo-button" size="mini" icon="el-icon-close" circle @click="item.completed = false;"></el-button>
+          <el-input class="todo-title" size="small" v-model="item.title"></el-input>
+          <el-button class="todo-button" size="mini" icon="el-icon-delete" circle @click="removeItem(item)"></el-button>
         </template>
       </div>
     </div>
@@ -75,5 +71,19 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+
+.todo-title {
+  border: 0px;
+  width: 200px;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.todo-button {
+  border: 0px;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
 </style>
